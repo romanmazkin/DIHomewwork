545785d22b22a90d826878a3d08cbe40f485a846
romanmazkin <romanmazkin@gmail.com>
2024-11-27 00:06:06 +0300

    Base entry point

---
+++ Assets/DIHomeworkProject/Develop/EntryPoint/Bootstrap.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+namespace Assets.DIHomeworkProject.Develop.EntryPoint
+{
+    public class Bootstrap : MonoBehaviour
+    {
+        public IEnumerator Run(DIContainer container)
+        {
+            //ILoadingCurrtain loadingCurrtain = container.Resolve<ILoadingCurrtain>();
+            //SceneSwitcher sceneSwitcher = container.Resolve<SceneSwitcher>();
+
+            //loadingCurrtain.Show();
+
+            Debug.Log("Run services initialization");
+
+            //initialize all services
+
+            //container.Resolve<PlayerDataProvider>().Load();
+
+            yield return new WaitForSeconds(1.5f);
+
+            //disable launch display
+
+            Debug.Log("Services initialization is done");
+
+            //loadingCurrtain.Hide();
+
+            //sceneSwitcher.ProcessSwitchSceneFor(new OutputBootstrapArgs(new MainMenuInputArgs()));
+        }
+    }
+}
---
+++ Assets/DIHomeworkProject/Develop/EntryPoint/EntryPoint.cs
@@ -0,0 +1,92 @@
+using UnityEngine;
+using Assets.DIHomeworkProject.Develop.EntryPoint;
+
+namespace Assets.CourseGame.Develop.EntryPoint
+{
+    public class EntryPoint : MonoBehaviour
+    {
+        [SerializeField] private Bootstrap _gameBootstrap;
+
+        private void Awake()
+        {
+            SetupAppSettings();
+
+            DIContainer projectContainer = new DIContainer();
+
+            // registration services for all project
+            // global context analog
+            // parent container creation
+
+            //RegisterResourcesAssetLoader(projectContainer);
+            //RegisterCoruotinePerrformer(projectContainer);
+
+            //RegisterLoadingCurtain(projectContainer);
+            //RegisterSceneLoader(projectContainer);
+            //RegisterSceneSwitcher(projectContainer);
+
+            //RegisterSaveLoadService(projectContainer);
+            //RegisterPlayerDataProvider(projectContainer);
+
+            //RegisterWalletService(projectContainer);
+
+            // all registrations done
+            //projectContainer.Initialize();
+
+            //projectContainer.Resolve<ICoroutinePerformer>().StartPerform(_gameBootstrap.Run(projectContainer));
+        }
+
+        private void SetupAppSettings()
+        {
+            QualitySettings.vSyncCount = 0;
+            Application.targetFrameRate = 144;
+        }
+
+        //    private void RegisterWalletService(DIContainer container)
+        //        => container.RegisterAsSingle(c => new WalletService(c.Resolve<PlayerDataProvider>())).NonLazy();
+
+        //    private void RegisterPlayerDataProvider(DIContainer container)
+        //        => container.RegisterAsSingle(c => new PlayerDataProvider(c.Resolve<ISaveLoadService>()));
+
+        //    private void RegisterResourcesAssetLoader(DIContainer container)
+        //         => container.RegisterAsSingle(c => new ResourcesAssetLoader());
+
+        //    private void RegisterCoruotinePerrformer(DIContainer container)
+        //    {
+        //        container.RegisterAsSingle<ICoroutinePerformer>(c =>
+        //        {
+        //            ResourcesAssetLoader resourcesAssetLoader = c.Resolve<ResourcesAssetLoader>();
+
+        //            CoroutinePerformer coroutinePerformerPrefab = resourcesAssetLoader.
+        //            LoadResource<CoroutinePerformer>(InfrastructureAssetPaths.CoroutinePerformerPath);
+
+        //            return Instantiate(coroutinePerformerPrefab);
+        //        });
+        //    }
+
+        //    private void RegisterLoadingCurtain(DIContainer container)
+        //    {
+        //        container.RegisterAsSingle<ILoadingCurrtain>(c =>
+        //        {
+        //            ResourcesAssetLoader resourcesAssetLoader = c.Resolve<ResourcesAssetLoader>();
+
+        //            StandartLoadingCurtain standartLoadingCurtainPrefab = resourcesAssetLoader.
+        //            LoadResource<StandartLoadingCurtain>(InfrastructureAssetPaths.StandartLoadingCurtainPath);
+
+        //            return Instantiate(standartLoadingCurtainPrefab);
+        //        });
+        //    }
+
+        //    private void RegisterSaveLoadService(DIContainer container)
+        //        => container.RegisterAsSingle<ISaveLoadService>(c => new SaveLoadService(new JsonSerializer(), new LocalDataRepository()));
+
+        //    private void RegisterSceneLoader(DIContainer container)
+        //        => container.RegisterAsSingle<ISceneLoader>(c => new DefaultSceneLoader());
+
+        //    private void RegisterSceneSwitcher(DIContainer container)
+        //        => container.RegisterAsSingle(c =>
+        //        new SceneSwitcher(c.Resolve<ICoroutinePerformer>(),
+        //            c.Resolve<ILoadingCurrtain>(),
+        //            c.Resolve<ISceneLoader>(),
+        //            c));
+    }
+}
